160136251989dbc29ce73ca0c6303bb5caf078e8
JemyDalsaniya <jemy.inexture@gmail.com>
2023-05-10 15:11:24 +0530

    Revert "deleted"
    This reverts commit 557c4986148fa5831d16616e25f427e1fcfe6ff2.

---
+++ src/main/java/com/example/springmicrosoftazureoauth2/SpringMicrosoftazureOauth2Application.java
@@ -0,0 +1,13 @@
+package com.example.springmicrosoftazureoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicrosoftazureOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicrosoftazureOauth2Application.class, args);
+	}
+
+}
